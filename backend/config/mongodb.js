f5b4cfae2e72a6334adb4dd1ede14efa59439739
Lavansi98 <sriansika3@gmail.com>
2024-11-03 15:32:33 +0530

    changes mongodb connection

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("DB connected");
   });
-  await mongoose.connect(`${process.env.MONGO_URI}/final-project`);
+  await mongoose.connect(`${process.env.MONGO_URI}`);
 };
 
 export default connectDB;

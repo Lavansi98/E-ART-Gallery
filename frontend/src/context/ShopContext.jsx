8d9656b0dd5f4e02e0826195945dcd9eaf204a85
Lavansi98 <sriansika3@gmail.com>
2024-11-04 16:05:42 +0530

    commit

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -7,7 +7,7 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
-    const currency = 'Rs  ';
+    const currency = 'Rs.  ';
     const delivery_fee = 200;
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search, setSearch] = useState('');

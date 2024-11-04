b6dbcfb1ae55fa96b728cb7e0cbdefb65d73bc5e
Lavansi98 <sriansika3@gmail.com>
2024-11-04 11:13:44 +0530

    updated latest collection and footer page

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import { assets } from '../assets/assets';
 import { FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
 
@@ -19,11 +20,11 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>EXPLORE</p>
           <ul className='flex flex-col gap-1 text-gray-300'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Terms of Service</li>
-            <li>Privacy Policy</li>
+            <li><Link to='/' className='hover:text-yellow-500'>Home</Link></li>
+            <li><Link to='/about' className='hover:text-yellow-500'>About</Link></li>
+            <li><Link to='/contact' className='hover:text-yellow-500'>Contact</Link></li>
+            <li><Link to='#' className='hover:text-yellow-500'>Terms of Service</Link></li>
+            <li><Link to='#' className='hover:text-yellow-500'>Privacy Policy</Link></li>
           </ul>
         </div>
 
@@ -31,16 +32,16 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>FOLLOW US</p>
           <div className='flex gap-4'>
-            <a href='#' className='text-gray-300 hover:text-white'>
+            <a href='#' className='text-gray-300 hover:text-yellow-500'>
               <FaFacebookF size={20} />
             </a>
-            <a href='#' className='text-gray-300 hover:text-white'>
+            <a href='#' className='text-gray-300 hover:text-yellow-500'>
               <FaInstagram size={20} />
             </a>
-            <a href='#' className='text-gray-300 hover:text-white'>
+            <a href='#' className='text-gray-300 hover:text-yellow-500'>
               <FaTwitter size={20} />
             </a>
-            <a href='#' className='text-gray-300 hover:text-white'>
+            <a href='#' className='text-gray-300 hover:text-yellow-500'>
               <FaLinkedinIn size={20} />
             </a>
           </div>
